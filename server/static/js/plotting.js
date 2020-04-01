fa409b23d494cd46c5dd49b6e6e20838e10a072b
Daniel Langesten <daniel.langest@gmail.com>
2020-04-01 12:59:52 +0100

    Fix day indexing

--- server/static/js/plotting.js
+++ server/static/js/plotting.js
@@ -79,7 +79,7 @@ var get_data_and_make_schedules_plot = function (chart_parent) {
 				var i;
 				data.x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 				for (i = 7; i >= 1; i--) {
-					data.y.push(day_names[i]);
+					data.y.push(day_names[i-1]);
 					var j;
 					var day_data = [];
 					for (j = 0; j < 24; j++) {
